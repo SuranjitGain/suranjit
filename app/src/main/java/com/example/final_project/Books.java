9e2dab93536892187a1669205a89c10c2836877d
Suranjit <sgain.10sg@gmail.com>
2020-12-15 23:35:32 +0600

    java file for  books

--- app/src/main/java/com/example/final_project/Books.java
+++ app/src/main/java/com/example/final_project/Books.java
@@ -2,13 +2,60 @@ package com.example.final_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Books extends AppCompatActivity {
-
+    Button home, Eng,kids,job,literature;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_books);
+
+        home= findViewById(R.id.homeid);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent( Books.this,MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+        Eng= findViewById(R.id.engid);
+        Eng.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent( Books.this,Eng_book.class);
+                startActivity(intent);
+            }
+        });
+
+        kids=(Button) findViewById(R.id.kidsid);
+        kids.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(Books.this,Kids.class);
+                startActivity(intent);
+            }
+        });
+        job=(Button)findViewById(R.id.jobid);
+        job.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(Books.this,job_book.class);
+                startActivity(intent);
+            }
+        });
+        literature=(Button)findViewById(R.id.literatureid);
+        literature.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(Books.this,literature_book.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
